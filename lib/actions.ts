930127249e509b412caf28b06866a7dd2e7f6503
VedankWakalkar <131476793+VedankWakalkar@users.noreply.github.com>
2025-06-25 13:40:20 +0530

    modified the action.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use server";
 
 import { Resend } from "resend";
@@ -7,19 +8,20 @@ import { ContactFormSchema } from "./schemas";
 const resend = new Resend(process.env.RESEND_API_KEY!);
 type ContactFormInputs = z.infer<typeof ContactFormSchema>;
 
-export async function sendEmail(data: ContactFormInputs) {
-  const parsed = ContactFormSchema.safeParse(data);
+export async function sendEmail(form: ContactFormInputs) {
+  const parsed = ContactFormSchema.safeParse(form);
   if (!parsed.success) return { error: parsed.error.format() };
 
   const { name, email, message } = parsed.data;
 
-  const { emailData, error } = await resend.emails.send({
-    from: "Vedank <onboarding@resend.dev>", // Sandbox sender
+  // Call Resend
+  const { data, error } = await resend.emails.send({
+    from: "Vedank <onboarding@resend.dev>",   // sandbox sender
     to: "vedankw11@gmail.com",
     replyTo: [email],
     cc: [email],
     subject: `New message from ${name}!`,
-    text: `Name:\n${name}\n\nEmail:\n${email}\n\nMessage:\n${message}`,
+    text: `Name:\n${name}\n\nEmail:\n${email}\n\nMessage:\n${message}`
   });
 
   if (error || !data) {
